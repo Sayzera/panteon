b9d214fa1760b7d688521be975d2d74da1b5fa11
Sezer Bölük <sezerboluk@Sezer-MacBook-Pro.local>
2022-05-04 17:10:52 +0300

    profile ve route düzenlendi

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname +  '/public')))
 
+app.get('*', (req,res) => {
+  return res.sendFile(path.join(__dirname + '/public', 'index.html'))
+})
+
 /**
  * Authentication
  */
@@ -35,7 +39,6 @@ app.options('*', cors())
 
 /**
  * route kontrol
- * eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjZjZjg1NmNlN2Q5NTQ3OWYwOGUzZjciLCJpYXQiOjE2NTE0MTUwNTUsImV4cCI6MTY1MTUwMTQ1NX0.szvN0C0WluxaklCV91Y98EO-5dtmSLky-oRmUB2Z0wQ
  */
 app.use(authJwt());
 app.use(errorHandler);
@@ -56,6 +59,8 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 app.use('/api/users',userRouter);
 app.use('/api/pools',poolRouter);
 app.use('/api/auth', authRouter);
+
+
 
 /**
  * listen to port
